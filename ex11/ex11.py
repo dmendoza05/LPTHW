03c496032eb8d89b86640a3e671f48f7e4b5c980
dmendoza05 <mendozad05@yahoo.com>
2015-07-14 16:19:34 -0700

    ex11

---
+++ ex11/ex11.py
@@ -0,0 +1,21 @@
+# print "How old are you?",
+# age = raw_input()
+# print "How tall are you?",
+# height = raw_input()
+# print "How much do you weigh?",
+# weight = raw_input()
+
+# print "So, you are %s old, %s tall, and %s heavy" % (age, height, weight)
+
+print "How much money do you have in your wallet?",
+money = '$%.2f' % float(raw_input())
+
+print "You have", money
+
+print "Give me a number:(integers)",
+a = int(raw_input())
+print "Give me a number:(float)",
+b = float(raw_input())
+
+ab = a + b
+print "I added the numbers and it's sum is", ab
