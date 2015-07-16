e5c09e3b94d583052d9733d6a4fdde5d001ffc82
dmendoza05 <mendozad05@yahoo.com>
2015-07-16 02:15:06 -0700

    ex14

---
+++ ex14/red_fruits.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, r1, r2, r3 = argv
+
+print "This is script: %s" % script
+print """We're only going to talk about red fruits!!
+So far I only have 3 red fruits
+They are %s, %s and %s""" % (r1, r2, r3)
