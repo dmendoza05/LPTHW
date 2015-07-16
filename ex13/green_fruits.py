c46d05498cb6b1e0d981717f1e51403b1dacd3ae
dmendoza05 <mendozad05@yahoo.com>
2015-07-16 00:23:17 -0700

    ex13

---
+++ ex13/green_fruits.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+g1, g2, g3, g4 = argv
+
+print "the first green fruit is:", g1
+print "the second green fruit is:", g2
+print "the third green fruit is:", g3
+print "the fourth green fruit is:", g4
+
+print "give me another green fruit:",
+g5 = raw_input()
+
+print "the fifth green fruit is:", g5
